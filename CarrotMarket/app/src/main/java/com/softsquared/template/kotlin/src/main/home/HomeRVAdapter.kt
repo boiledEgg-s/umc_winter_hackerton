8cac79b5493df8d4159bed18d0064f4ae58af64a
yoonjin <peacemiller@konkuk.ac.kr>
2022-12-22 22:08:35 +0900

    10:08 commit again

--- CarrotMarket/app/src/main/java/com/softsquared/template/kotlin/src/main/home/HomeRVAdapter.kt
+++ CarrotMarket/app/src/main/java/com/softsquared/template/kotlin/src/main/home/HomeRVAdapter.kt
@@ -1,4 +1,34 @@
 package com.softsquared.template.kotlin.src.main.home
 
-class HomeRVAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.softsquared.template.kotlin.databinding.HomeRvLayoutBinding
+
+class HomeRVAdapter(private val itemList: ArrayList<item>) :
+    RecyclerView.Adapter<HomeRVAdapter.ViewHolder>() {
+    inner class ViewHolder(private val binding: HomeRvLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        //item 객체의 데이터를 연결함
+        fun bind(data: item) {
+            binding.image.setImageResource(data.image)
+            binding.title.text = data.title
+            binding.location.text = data.loction
+            binding.time.text = data.time
+            binding.price.text = data.price.toString()
+            binding.chatNum.text = data.chatNum.toString()
+            binding.heartNum.text = data.heartNum.toString()
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = HomeRvLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(itemList[position])
+    }
+
+    override fun getItemCount(): Int = itemList.size
 }
